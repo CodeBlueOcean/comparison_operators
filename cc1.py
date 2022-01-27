5ff9fb14656950bf38f175128d9e66504db1b5a1
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-01-26 23:00:08 -0600

    Add files via upload

---
+++ cc1.py
@@ -0,0 +1,16 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 2.0
+print(type(b))
+
+c = "3.0"
+print(type(c))
+
+d = a + b 
+print(d)
+
---
+++ comparison_operators.py
@@ -0,0 +1,23 @@
+"""
+Python Comparison Operators
+"""
+X = 100
+Y = 200
+
+print( X != Y )          # X is not equal to Y
+print( X > Y )           # X is greater than Y
+print( X < Y )           # X is less than Y
+print( X >= Y )          # X is greater than or equal to Y
+print( X <= Y )          # X is less than or equal to Y
+
+"""
+Logical Operators
+"""
+X = 100
+Y = 200
+Z = 100
+
+print (X < Y and Y > Z)
+print (X > Y or Y > Z)
+print(not(X > Y or Y > Z))
+
